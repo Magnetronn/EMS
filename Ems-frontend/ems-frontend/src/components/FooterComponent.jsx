3fe56bcc128a08535edc9815b242d2b1c91041b4
Magnetronn <shivampatelvns21@gmail.com>
2024-04-14 13:07:36 +0530

    ems frontend added

---
+++ Ems-frontend/ems-frontend/src/components/FooterComponent.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const FooterComponent = () => {
+  return (
+    <div>
+        <footer className='footer'>
+            <span>All rights reserved 2023 by javaGuide</span>
+        </footer>
+    </div>
+  )
+}
+
+export default FooterComponent
